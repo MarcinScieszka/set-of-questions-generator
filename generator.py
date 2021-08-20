5cea7965b6a0770b263ab498c00c2b0d2eccbd28
MarcinScieszka <79609951+MarcinScieszka@users.noreply.github.com>
2021-08-20 20:47:17 +0200

    generate sets of questions form provided file number of sets and questions are predefined

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 import re
+import random
 
 def generate_demo_questions(num):
     """generate demo questions"""
@@ -23,6 +24,26 @@ def main():
     # generate_demo_questions(5)
     questions = get_questions()
 
+    # TODO: user defined numbers
+    number_of_questions_in_set = 3
+    number_of_sets = 20
+
+    set_of_sets_of_questions = []
+    current_set_of_questions = []
+
+    for i in range(1, number_of_sets*number_of_questions_in_set+1):
+        quesion_id = random.randint(0, len(questions)-1)
+        current_set_of_questions.append(questions[quesion_id])
+
+        if i % number_of_questions_in_set == 0:
+            set_of_sets_of_questions.append(current_set_of_questions)
+            current_set_of_questions = []
+    
+    for num, set in enumerate(set_of_sets_of_questions):
+        print(f'\nSet nr {num+1}')
+        for question in set:
+            print(f'{question}')
+            
 
 if __name__ == "__main__":
     main()
