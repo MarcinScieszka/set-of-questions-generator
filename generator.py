13b33711673b33fd514074d05d1a30dc398a0446
MarcinScieszka <79609951+MarcinScieszka@users.noreply.github.com>
2021-08-20 19:31:56 +0200

    add get_questions function returns list of questions after removing enumeration

--- generator.py
+++ generator.py
@@ -1,6 +1,4 @@
-
-def main():
-   generate_demo_questions(5)
+import re
 
 def generate_demo_questions(num):
     """generate demo questions"""
@@ -8,6 +6,23 @@ def generate_demo_questions(num):
     with open('demo-questions.txt', 'w') as f:
         for i in range(1, num+1):
             f.write(f'{i}. question nr {i}\n')
+
+def get_questions():
+    """read quesions from file and remove enumeration"""
+    questions = []
+    
+    with open('demo-questions.txt', 'r') as q:
+        for question in q:
+            q1 = question.strip()
+            q2 = re.sub(r'^.*?\. ', '', q1)
+            questions.append(q2)
+
+    return questions
+
+def main():
+    # generate_demo_questions(5)
+    questions = get_questions()
+
 
 if __name__ == "__main__":
     main()
